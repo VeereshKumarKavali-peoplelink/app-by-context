9eecaee0cb47c2aca87c723bb78c5c3c99636ba8
VeereshKumarKavali-peoplelink <veereshk@peoplelinkvc.com>
2023-03-29 13:08:25 +0530

    adding my code to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react'
+import LanguageContext from './context/LanguageContext'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import Header from './components/Header'
+import LandingSection from './components/LandingSection'
+import FeaturesSection from './components/FeaturesSection'
+
+class App extends Component {
+  state = {activeLanguage: 'EN'}
+
+  changeLanguage = activeLanguage => {
+    this.setState({activeLanguage})
+  }
+
+  render() {
+    const {activeLanguage} = this.state
+    return (
+      <>
+        <LanguageContext.Provider
+          value={{activeLanguage, changeLanguage: this.changeLanguage}}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Header />
+          <LandingSection />
+          <FeaturesSection />
+        </LanguageContext.Provider>
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
+
+
+
+
+
+
+
